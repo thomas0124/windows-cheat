9fa77ffbab1759e67fa8da9f1dc2bf51e53455a6
トーマス <210441057@ccmailg.meijo-u.ac.jp>
2023-10-27 22:33:26 +0900

    [fix]flexboxの修正

--- popup.js
+++ popup.js
@@ -10,18 +10,21 @@ document.addEventListener('DOMContentLoaded', function() {
         .then(function(commands) {
             // 読み込んだデータを使用してselect要素にオプションを追加する
             for (var command in commands) {
-                var option = document.createElement('option');
-                option.value = command;
-                option.textContent = command;
-                commandList.appendChild(option);
-            }
+                var commandDiv = document.createElement('div');
+                commandDiv.className = 'command-item';
 
-            // ユーザーがコマンドを選択したときの処理を定義
-            commandList.addEventListener('change', function() {
-                var selectedCommand = commandList.value;
-                var description = commands[selectedCommand];
-                commandDescription.textContent = description;
-            });
+                var keySpan = document.createElement('span');
+                keySpan.className = 'command-key';
+                keySpan.textContent = command;
+
+                var valueSpan = document.createElement('span');
+                valueSpan.className = 'command-value';
+                valueSpan.textContent = commands[command];
+
+                commandDiv.appendChild(keySpan);
+                commandDiv.appendChild(valueSpan);
+                commandList.appendChild(commandDiv);
+            }
         })
         .catch(function(error) {
             console.error('データの読み込み中にエラーが発生しました:', error);
